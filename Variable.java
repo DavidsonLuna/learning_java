8565605b37a9b242c8a1cbd64d86f812b77c6dc6
Deividson Ferreira de Luna <dfluna@walmart.com.br>
2017-12-20 15:12:29 -0200

    Add class Variable

---
+++ Variable.java
@@ -0,0 +1,23 @@
+class Variable {
+    public static void main (String[]args) {
+        int johnAge = 18;
+        int maryAge = 21;
+
+        int sumAge = johnAge + maryAge;
+        System.out.println(sumAge);
+
+        double pi = 3.1415;
+        double doublePi = pi * 2;
+
+        System.out.println(doublePi);
+
+        boolean friend = true;
+        boolean enemy = !friend;
+
+        boolean mlda = johnAge <= 18; //Minimum Legal Drinking Age (MLDA)
+
+        char letter = 'M';
+        String completName = "Joao da Silva";
+        System.out.println(completName);
+    }
+}k
